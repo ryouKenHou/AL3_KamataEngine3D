0eeb20bf466d6ca94e493ceffe701099b08607c7
ryouKenHou <k025g0112@g.neec.ac.jp>
2026-06-30 14:20:10 +0900

    AL3_05_13: fade

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,15 +1,6 @@
 #include "TitleScene.h"
 
-
-void TitleScene::Draw() {
-	KamataEngine::Model::PreDraw();
-	// タイトルシーンの描画処理
-	playerModel_->Draw(playerWorldTransform_, *camera_);
-	titleModel_->Draw(titleWorldTransform_, *camera_);
-	KamataEngine::Model::PostDraw();	
-}
-
-void TitleScene::Initialize() {	
+void TitleScene::Initialize() {
 	// タイトルシーンの初期化処理
 	playerModel_ = KamataEngine::Model::CreateFromOBJ("player", true);
 	playerWorldTransform_.Initialize();
@@ -19,12 +10,16 @@ void TitleScene::Initialize() {
 	titleModel_ = KamataEngine::Model::CreateFromOBJ("titleFont", true);
 	titleWorldTransform_.Initialize();
 	titleWorldTransform_.translation_ = {0.f, 5.f, -1.f};
-	//titleWorldTransform_.rotation_.y = 3.141592654f;
+	// titleWorldTransform_.rotation_.y = 3.141592654f;
 	titleWorldTransform_.scale_ = {1.5f, 1.5f, 1.5f};
 
 	camera_ = new KamataEngine::Camera();
 	camera_->Initialize();
 	camera_->translation_.z = -15.f;
+
+	fade_ = new Fade();
+	fade_->Initialize();
+	fade_->Start(Fade::Status::kFadeIn, 1.0f);
 }
 
 void TitleScene::Update() {
@@ -33,13 +28,39 @@ void TitleScene::Update() {
 	playerWorldTransform_.matWorld_ = CreateAffineMatrix(playerWorldTransform_.scale_, playerWorldTransform_.rotation_, playerWorldTransform_.translation_);
 	playerWorldTransform_.TransferMatrix();
 
-
-	 titleWorldTransform_.translation_.y += sinf(frameCounter_ * 0.05f) * 0.08f;
+	titleWorldTransform_.translation_.y += sinf(frameCounter_ * 0.05f) * 0.08f;
 
 	titleWorldTransform_.matWorld_ = CreateAffineMatrix(titleWorldTransform_.scale_, titleWorldTransform_.rotation_, titleWorldTransform_.translation_);
 	titleWorldTransform_.TransferMatrix();
 
-	if (KamataEngine::Input::GetInstance()->TriggerKey(DIK_SPACE)) {
-		isFinished_ = true;
+	switch (phase_) {
+	case Phase::kFadeIn:
+		if (fade_->IsFinished()) {
+			phase_ = Phase::kMain;
+		}
+		break;
+	case Phase::kMain:
+		if (KamataEngine::Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+			phase_ = Phase::kFadeOut;
+			fade_->Start(Fade::Status::kFadeOut, 1.0f);
+		}
+		break;
+	case Phase::kFadeOut:
+		if (fade_->IsFinished()) {
+			isFinished_ = true;
+		}
+		break;
 	}
+
+	fade_->Update();
+}
+
+void TitleScene::Draw() {
+	KamataEngine::Model::PreDraw();
+	// タイトルシーンの描画処理
+	playerModel_->Draw(playerWorldTransform_, *camera_);
+	titleModel_->Draw(titleWorldTransform_, *camera_);
+	KamataEngine::Model::PostDraw();
+
+	fade_->Draw();
 }
